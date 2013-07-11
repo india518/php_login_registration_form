76c81902e2073e5863595e274e473bfc929a7e4a
India Meisner <india.rae@gmail.com>
2013-07-11 14:23:14 -0700

    Refactor all views per video feedback; more streamlined form.

--- index.php
+++ index.php
@@ -11,76 +11,67 @@
 	<body>
 		<div id="wrapper">
 
-			<div id="testing" class="hidden">
-				<!-- hide this for now; may need it for debugging later -->
-				<?php
-					echo "<p>" . $_SESSION["birth_date"] . "</p>";
-				?>
-			</div>
-
-	<?php
-			if ( isset($_SESSION["errors"]) )
-			{
-				//display all error messages in "errors" array
-				foreach($_SESSION["errors"] as $error)
+			<div id="display_messages"
+			<?php
+				if ( isset( $_SESSION["success_message"]) )
 				{	?>
-		 			<h3 id="error_display">
-	<?php		 		echo $error;	?>
-					</h3>	
-	<?php		}	
-			}	?>
+					class="success_border" </h3>
+			<?php	echo "<h3>{$_SESSION['success_message']}</h3>";
 
-	<?php
-			if ( isset($_SESSION["success"]) )
-			{	?>
-		 		<h3 id="registration_success">
-	<?php			echo $_SESSION["success"];	?>
-				</h3>		
-	<?php	}	?>
+				}
+				else if ( isset( $_SESSION["error_messages"]) )
+				{	?>
+					class="error_border" </h3>
+			<?php	foreach( $_SESSION["error_messages"] as $message )
+					{
+						echo "<h3>{$message}</h3>";
+					}
+				}	?>
+			</div>
 
 			<form id="registration" action="process.php" method="post">
 				<div>
 					<label for="first_name">* First Name: </label>
-					<input type="text" name="first_name" id="first_name"
-					<?= ( isset($_SESSION["errors"]["first_name"]) ) ?
-							" class='error_highlight'" : ""	?> />
+					<input type="text" name="first_name" id="first_name" placeholder="First Name" 
+					<?= ( isset($_SESSION["error_messages"]["first_name"]) ) ?
+							" class='highlight'" : ""	?> />
 				</div>
 				<div>
 					<label for="last_name">* Last Name: </label>
-					<input type="text" name="last_name" id="last_name"
-					<?= ( isset($_SESSION["errors"]["last_name"]) ) ?
-							" class='error_highlight'" : ""	?> />
+					<input type="text" name="last_name" id="last_name" placeholder="Last Name" 
+					<?= ( isset($_SESSION["error_messages"]["last_name"]) ) ?
+							" class='highlight'" : ""	?> />
 				</div>
 				<div>
 					<label for="email">* Email: </label>
-					<input type="text" name="email" id="email"
-					<?= ( isset($_SESSION["errors"]["email"]) ) ?
-							" class='error_highlight'" : ""	?> />
+					<input type="text" name="email" id="email" placeholder="Email Address" 
+					<?= ( isset($_SESSION["error_messages"]["email"]) ) ?
+							" class='highlight'" : ""	?> />
 				</div>
 				<div>		
 					<label for="birth_date">Birth Date: </label>
-					<input type="date" name="birth_date" id="birth_date"
-					<?= ( isset($_SESSION["errors"]["birth_date"]) ) ?
-							" class='error_highlight'" : ""	?> />
+					<input type="date" name="birth_date" id="birth_date" 
+					<?= ( isset($_SESSION["error_messages"]["birth_date"]) ) ?
+							" class='highlight'" : ""	?> />
 				</div>
 				<div>
 					<label for="password">* Password: </label>
-					<input type="password" name="password" id="password"
-					<?= ( isset($_SESSION["errors"]["password"]) ) ?
-							" class='error_highlight'" : ""	?> />
+					<input type="password" name="password" id="password" placeholder="password" 
+					<?= ( isset($_SESSION["error_messages"]["password"]) ) ?
+							" class='highlight'" : ""	?> />
 				</div>
 				<div>
 					<label for="confirm_password">* Confirm Password: </label>
-					<input type="password" name="confirm_password" id="confirm_password"
-					<?= ( isset($_SESSION["errors"]["confirm_password"]) ) ?
-							" class='error_highlight'" : ""	?> />
+					<input type="password" name="confirm_password" id="confirm_password" placeholder="confirm password" 
+					<?= ( isset($_SESSION["error_messages"]["confirm_password"]) ) ?
+							" class='highlight'" : ""	?> />
 				</div>
-				<input type="submit" value="Login" />
+				<input type="submit" value="Register" />
 			</form>
 		</div>
 	</body>
 </html>
 <?php
-	unset($_SESSION["errors"]);
-	unset($_SESSION["success"]);
+	unset($_SESSION["success_message"]);
+	unset($_SESSION["error_messages"]);
 ?>
--- process.php
+++ process.php
@@ -1,71 +1,79 @@
 <?php
 session_start();
 
-	$min_password_length = 6;
+	if($_POST)
+	{
+		// $errors = array();
+		$errors = NULL;
 
-	if ( empty($_POST["first_name"]) )
-	{
-		$_SESSION["errors"]["first_name"] = "The First Name field is empty!";
-	}
-	if ( empty($_POST["last_name"]) )
-	{
-		$_SESSION["errors"]["last_name"] = "The Last Name field is empty!";
-	}
-	if ( empty($_POST["email"]) )
-	{
-		$_SESSION["errors"]["email"] = "The Email field is empty!";
+		//First name validation
+		if ( empty($_POST["first_name"]) )
+		{
+			$errors["first_name"] = "First Name cannot be blank.";
+		}
+		else if ( is_numeric($_POST["first_name"]) )
+		{
+			$errors["first_name"] = "First Name cannot contain numbers.";
+		}
+
+		//Last name validation
+		if ( empty($_POST["last_name"]) )
+		{
+			$errors["last_name"] = "Last Name cannot be blank.";
+		}
+		else if ( is_numeric($_POST["last_name"]) )
+		{
+			$errors["last_name"] = "Last Name cannot contain numbers.";
+		}
+
+		//Email validation
+		if ( empty($_POST["email"]) )
+		{
+			$errors["email"] = "Email address cannot be blank.";
+		}
+		else if ( filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == FALSE )
+		{
+			$errors["email"] = "Email should be in valid format.";
+		}
+
+		//Birthdate validation
+		// more useful if this is text field and not a datepicker,
+		// but I'll leave this in for reference.
+		if( !empty($_POST["birth_date"]) and !preg_match(" '\b(0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2}\b' ", $_POST["birth_date"]) )
+		{
+			$errors["birth_date"] = "Birth Date should be in valid format.";
+		}
+
+		// Password
+		if( empty($_POST["password"]) )
+		{
+			$errors["password"] = "Password cannot be blank.";
+		}
+		else if ( strlen($_POST["password"]) < 6 )
+		{
+			$errors["password"] = "Password should be at least 6 characters long";
+		}
+
+		//Confirm password validation
+		if ( empty($_POST["confirm_password"]) )
+		{
+			$errors["confirm_password"] = "The Confirm Password field cannot be blank.";
+		}
+		else if ( $_POST["confirm_password"] != $_POST["password"] )
+		{
+			$errors["confirm_password"] = "Passwords do not match.";
+		}
+
+		if($errors == NULL)
+		{
+			$_SESSION["success_message"] = "Thank you for submitting your form!";
+		}
+		else
+		{
+			$_SESSION["error_messages"] = $errors;
+		}
 	}
 	
-	if ( empty($_POST["password"]) )
-	{
-		$_SESSION["errors"]["password"] = "The password field is empty!";
-	}
-	else if ( strlen($_POST["password"]) <= $min_password_length )
-	{	
-		if ( strlen($_POST["password"]) <= $min_password_length )
-		//we have a password, check the length
-		$_SESSION["errors"]["password"] = "The password should be more than 6 characters!";
-	}
-
-	//Maybe this should be refactored into one function,
-	//called twice?
-	if ( preg_match("#[\d]#", $_POST["first_name"]) )
-	{
-		$_SESSION["errors"]["first_name"] = "First name is invalid!";
-		// Remember that you can't have a digit in an empty field!
-	}
-	if ( preg_match("#[\d]#", $_POST["last_name"]) )
-	{
-		$_SESSION["errors"]["last_name"] = "Last name is invalid!";
-	}
-
-	//validate email
-	if(! (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) )
-	{
-		$_SESSION["errors"]["email"] =  "Invalid email!";
-	}
-
-	//validate birth_date
-	// May come back to this - if we use the date picker,
-	// this seems to validate itself before submission!
-	// otherwise - use regex to check?
-	if ( isset($_POST["birth_date"]) )
-	{
-		$birth_date = $_POST["birth_date"]; //for display purposes
-	}
-
-	//validate password = confirm_password
-	if (! ($_POST["password"] == $_POST["confirm_password"]) )
-	{
-		$_SESSION["errors"]["confirm_password"] =  "The password fields do not match!";
-	}
-
-	if (! isset($_SESSION["errors"]) )
-	{
-		$_SESSION["success"] = "Thanks for submitting your information.";
-	}
-
-	//$_SESSION["birth_date"] = $birth_date;
 	header("location: index.php");
 
 ?>
